5cd27b8f2c9a92ac9a0b3cf8f6fe7f5e49a6e085
Kenjebai <142065688+Kenjebai@users.noreply.github.com>
2023-09-16 15:17:43 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+
+const App = () => {
+  const [color1, setColor1] = useState("red")
+  const [color2, setColor2] = useState("blue")
+
+  function swapColors(){
+    setColor1(color2)
+    setColor2(color1)
+  }
+
+  return(
+  <div>
+    <div style={{width:"200px", height:"200px", backgroundColor:color1}} onClick={swapColors}></div>
+    <div style={{width:"200px", height:"200px", backgroundColor:color2}} onClick={swapColors}></div>
+  </div>)
+
+}
+
+export default App
